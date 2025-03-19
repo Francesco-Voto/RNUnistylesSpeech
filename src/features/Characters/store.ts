33c9b759ce00f3a46e2c7af02ecd00d72b0592a1
Francesco-Voto <francesco.voto@nearform.com>
2025-03-19 17:02:39 +0100

    fix: storage for favourite check

--- src/features/Characters/store.ts
+++ src/features/Characters/store.ts
@@ -1,5 +1,7 @@
-import {Character} from '@services/api';
+import {type Character} from '@services/api';
+import {MMKV} from 'react-native-mmkv';
 import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
 
 type State = {
   favourites: number[];
@@ -9,22 +11,38 @@ type State = {
   removeAllFavourites: () => void;
 };
 
-const useCharactersStore = create<State>((set, get) => ({
-  favourites: [],
-  setFavourite: character => {
-    const favArray = get().favourites;
-    favArray.push(character.id);
-    set({favourites: favArray});
-  },
-  removeFromFavourites: character => {
-    const favArray = get().favourites;
-    set({favourites: favArray.filter(id => character.id !== id)});
-  },
-  isFavourite: character => {
-    const favArray = get().favourites;
-    return favArray.some(id => id === character);
-  },
-  removeAllFavourites: () => set({favourites: []}),
-}));
+const storage: MMKV = new MMKV();
+export const createJSONStorageHelper = (jsonStorage: MMKV) =>
+  createJSONStorage(() => ({
+    getItem: key => jsonStorage.getString(key) || null,
+    setItem: (key, value) => jsonStorage.set(key, value),
+    removeItem: key => jsonStorage.delete(key),
+  }));
+
+const useCharactersStore = create<State>()(
+  persist(
+    (set, get) => ({
+      favourites: [],
+      setFavourite: character => {
+        const favArray = get().favourites;
+        favArray.push(character.id);
+        set({favourites: favArray});
+      },
+      removeFromFavourites: character => {
+        const favArray = get().favourites;
+        set({favourites: favArray.filter(id => character.id !== id)});
+      },
+      isFavourite: character => {
+        const favArray = get().favourites;
+        return favArray.some(id => id === character);
+      },
+      removeAllFavourites: () => set({favourites: []}),
+    }),
+    {
+      name: 'character-state',
+      storage: createJSONStorageHelper(storage),
+    },
+  ),
+);
 
 export default useCharactersStore;
